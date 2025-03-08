51b1e611a53fb8c7304b308568155a6067572684
ToffoluttiVittorio <vittorio.toffolutti@gmail.com>
2025-03-08 21:56:28 +0100

    Spring initialzr

---
+++ backend/app-compta/src/main/java/com/app_compta/app_compta/AppComptaApplication.java
@@ -0,0 +1,13 @@
+package com.app_compta.app_compta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppComptaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppComptaApplication.class, args);
+	}
+
+}
